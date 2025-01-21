51ed15d1dfa3964f48c1d5e794ba44288a1e4ff6
Iago Diniz <iago.diniz71@gmail.com>
2025-01-21 19:35:37 -0300

    Adicionando arquivo aula01 Console Log

---
+++ Aula01_ConsoleLog.js
@@ -0,0 +1,8 @@
+// Normalmente o console.log() é utilizado para debugar o código, ou seja, para verificar se o código está funcionando corretamente.
+console.log("Olá, Mundo!");
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(2, 3)); // 5
