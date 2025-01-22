740d9b5d09597ecd3a8a7737d06229f892f884ea
Iago Diniz <iago.diniz71@gmail.com>
2025-01-22 17:07:48 -0300

    Adicionando arquivos aula 02 - Let

--- Aula01_ConsoleLog.js
+++ Aula01_ConsoleLog.js
@@ -1,8 +1,4 @@
 // Normalmente o console.log() é utilizado para debugar o código, ou seja, para verificar se o código está funcionando corretamente.
 console.log("Olá, Mundo!");
-
-function sum(a, b) {
-    return a + b;
-}
-
-console.log(sum(2, 3)); // 5
+console.log("Este é um exemplo de console.log()");
+console.log(12, 50.10, "Olá, Mundo!");
---
+++ Aula02_VariáveisLet.js
@@ -0,0 +1,13 @@
+// Let - É usado para declarar variáveis locais em um escopo de bloco, instrução ou expressão.
+// usado para deixar a variável guardada em um escopo local, ou seja, dentro de um bloco de código.
+
+let nome = "Iago Diniz";
+console.log("Meu nome é", nome);
+
+
+let nome2;
+nome2 = "Iago";
+console.log("Meu nome é", nome2);
+nome2 = "Enzo"
+console.log("Meu nome é", nome2);
+// O let é utilizado para declarar variáveis locais em um escopo de bloco, instrução ou expressão.
